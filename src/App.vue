eb39afdb4150eb567faf4423c96eabf668657cc0
dkliao <dklassic@gmail.com>
2025-05-23 22:16:24 +0900

    feat: Implement full fledge bookmark manager

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BookmarkManager from './components/BookmarkManager.vue'
+</script>
+
+<template>
+  <div class="app">
+    <BookmarkManager />
+  </div>
+</template>
+
+<style></style>
